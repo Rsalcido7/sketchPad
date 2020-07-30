e982ae3a08f11733a7730b6f51f485d425492587
Rsalcido <raulalexsalcido@gmail.com>
2020-07-29 20:45:17 -0600

    Added the Base Files to the SkethPad project

---
+++ app.js
@@ -0,0 +1,17 @@
+let container = document.getElementById("container");
+let gridSq  = document.createElement("div");
+gridSq.classList.add("gridSq");
+let sqNumber = " ";
+
+
+function boxNum {
+    for (let i = 1 ; i =sqNumber ; i++)
+}
+
+function create() {
+    container.appendChild(gridSq);
+}
+
+
+
+
